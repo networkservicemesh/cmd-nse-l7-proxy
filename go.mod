d9cf229e638cc5abb234878f79de29eecf77a52b
Sergey Shlyanin <sergey.shlyanin@xored.com>
2022-05-10 13:35:23 +0700

    Clone cmd-nse-icmp-responder repository
    Signed-off-by: Sergey Shlyanin <sergey.shlyanin@xored.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
-module github.com/networkservicemesh/cmd-template
+module github.com/networkservicemesh/cmd-nse-icmp-responder
 
 go 1.16
+
+require (
+	github.com/antonfisher/nested-logrus-formatter v1.3.1
+	github.com/edwarnicke/exechelper v1.0.2
+	github.com/edwarnicke/grpcfd v1.1.2
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/networkservicemesh/api v1.3.0-rc.1.0.20220405210054-fbcde048efa5
+	github.com/networkservicemesh/sdk v0.5.1-0.20220507173809-41196bdf49db
+	github.com/networkservicemesh/sdk-sriov v0.0.0-20220507174402-1f53cfa20170
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.8.1
+	github.com/spiffe/go-spiffe/v2 v2.0.0
+	github.com/stretchr/testify v1.7.0
+	google.golang.org/grpc v1.42.0
+	gopkg.in/yaml.v2 v2.4.0
+)
--- main.go
+++ main.go
@@ -1,4 +1,5 @@
-// Copyright (c) 2021 Doc.ai and/or its affiliates.
+// Copyright (c) 2020-2022 Doc.ai and/or its affiliates.
+// Copyright (c) 2021-2022 Nordix and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -14,7 +15,386 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build linux
+// +build linux
+
 package main
 
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/url"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"sync/atomic"
+	"syscall"
+	"time"
+
+	"gopkg.in/yaml.v2"
+
+	nested "github.com/antonfisher/nested-logrus-formatter"
+	"github.com/edwarnicke/grpcfd"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	kernelmech "github.com/networkservicemesh/api/pkg/api/networkservice/mechanisms/kernel"
+	"github.com/networkservicemesh/api/pkg/api/networkservice/mechanisms/noop"
+	registryapi "github.com/networkservicemesh/api/pkg/api/registry"
+	"github.com/networkservicemesh/sdk-sriov/pkg/networkservice/common/token"
+	"github.com/networkservicemesh/sdk-sriov/pkg/tools/tokens"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/chains/endpoint"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/authorize"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/mechanisms"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/mechanisms/kernel"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/mechanisms/recvfd"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/mechanisms/sendfd"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/null"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/onidle"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/policyroute"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/connectioncontext/dnscontext"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/chain"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/ipam/point2pointipam"
+	registryclient "github.com/networkservicemesh/sdk/pkg/registry/chains/client"
+	registrysendfd "github.com/networkservicemesh/sdk/pkg/registry/common/sendfd"
+	"github.com/networkservicemesh/sdk/pkg/tools/debug"
+	dnstools "github.com/networkservicemesh/sdk/pkg/tools/dnscontext"
+	"github.com/networkservicemesh/sdk/pkg/tools/fs"
+	"github.com/networkservicemesh/sdk/pkg/tools/grpcutils"
+	"github.com/networkservicemesh/sdk/pkg/tools/log"
+	"github.com/networkservicemesh/sdk/pkg/tools/log/logruslogger"
+	"github.com/networkservicemesh/sdk/pkg/tools/opentelemetry"
+	"github.com/networkservicemesh/sdk/pkg/tools/spiffejwt"
+	"github.com/networkservicemesh/sdk/pkg/tools/tracing"
+)
+
+// Config holds configuration parameters from environment variables
+type Config struct {
+	Name                  string            `default:"icmp-server" desc:"Name of ICMP Server"`
+	BaseDir               string            `default:"./" desc:"base directory" split_words:"true"`
+	ConnectTo             url.URL           `default:"unix:///var/lib/networkservicemesh/nsm.io.sock" desc:"url to connect to" split_words:"true"`
+	MaxTokenLifetime      time.Duration     `default:"10m" desc:"maximum lifetime of tokens" split_words:"true"`
+	ServiceNames          []string          `default:"icmp-responder" desc:"Name of provided services" split_words:"true"`
+	Payload               string            `default:"ETHERNET" desc:"Name of provided service payload" split_words:"true"`
+	Labels                map[string]string `default:"" desc:"Endpoint labels"`
+	DNSConfigs            dnstools.Decoder  `default:"[]" desc:"DNSConfigs represents array of DNSConfig in json format. See at model definition: https://github.com/networkservicemesh/api/blob/main/pkg/api/networkservice/connectioncontext.pb.go#L426-L435" split_words:"true"`
+	CidrPrefix            []string          `default:"169.254.0.0/16" desc:"List of CIDR Prefix to assign IPv4 and IPv6 addresses from" split_words:"true"`
+	IdleTimeout           time.Duration     `default:"0" desc:"timeout for automatic shutdown when there were no requests for specified time. Set 0 to disable auto-shutdown." split_words:"true"`
+	RegisterService       bool              `default:"true" desc:"if true then registers network service on startup" split_words:"true"`
+	PBRConfigPath         string            `default:"/etc/policy-based-routing/config.yaml" desc:"Path to policy based routing config file" split_words:"true"`
+	LogLevel              string            `default:"INFO" desc:"Log level" split_words:"true"`
+	OpenTelemetryEndpoint string            `default:"otel-collector.observability.svc.cluster.local:4317" desc:"OpenTelemetry Collector Endpoint"`
+}
+
+// Process prints and processes env to config
+func (c *Config) Process() error {
+	if err := envconfig.Usage("nsm", c); err != nil {
+		return errors.Wrap(err, "cannot show usage of envconfig nse")
+	}
+	if err := envconfig.Process("nsm", c); err != nil {
+		return errors.Wrap(err, "cannot process envconfig nse")
+	}
+	return nil
+}
+
 func main() {
+	// ********************************************************************************
+	// setup context to catch signals
+	// ********************************************************************************
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		// More Linux signals here
+		syscall.SIGHUP,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+	defer cancel()
+
+	// ********************************************************************************
+	// setup logging
+	// ********************************************************************************
+	log.EnableTracing(true)
+	logrus.SetFormatter(&nested.Formatter{})
+	ctx = log.WithLog(ctx, logruslogger.New(ctx, map[string]interface{}{"cmd": os.Args[0]}))
+
+	if err := debug.Self(); err != nil {
+		log.FromContext(ctx).Infof("%s", err)
+	}
+
+	// enumerating phases
+	log.FromContext(ctx).Infof("there are 6 phases which will be executed followed by a success message:")
+	log.FromContext(ctx).Infof("the phases include:")
+	log.FromContext(ctx).Infof("1: get config from environment")
+	log.FromContext(ctx).Infof("2: retrieve spiffe svid")
+	log.FromContext(ctx).Infof("3: create icmp server ipam")
+	log.FromContext(ctx).Infof("4: create icmp server nse")
+	log.FromContext(ctx).Infof("5: create grpc and mount nse")
+	log.FromContext(ctx).Infof("6: register nse with nsm")
+	log.FromContext(ctx).Infof("a final success message with start time duration")
+
+	starttime := time.Now()
+
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("executing phase 1: get config from environment")
+	// ********************************************************************************
+	config := new(Config)
+	if err := config.Process(); err != nil {
+		logrus.Fatal(err.Error())
+	}
+	l, err := logrus.ParseLevel(config.LogLevel)
+	if err != nil {
+		logrus.Fatalf("invalid log level %s", config.LogLevel)
+	}
+	logrus.SetLevel(l)
+
+	log.FromContext(ctx).Infof("Config: %#v", config)
+
+	// ********************************************************************************
+	// Configure Open Telemetry
+	// ********************************************************************************
+	if opentelemetry.IsEnabled() {
+		collectorAddress := config.OpenTelemetryEndpoint
+		spanExporter := opentelemetry.InitSpanExporter(ctx, collectorAddress)
+		metricExporter := opentelemetry.InitMetricExporter(ctx, collectorAddress)
+		o := opentelemetry.Init(ctx, spanExporter, metricExporter, config.Name)
+		defer func() {
+			if err = o.Close(); err != nil {
+				log.FromContext(ctx).Error(err.Error())
+			}
+		}()
+	}
+
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("executing phase 2: retrieving svid, check spire agent logs if this is the last line you see")
+	// ********************************************************************************
+	source, err := workloadapi.NewX509Source(ctx)
+	if err != nil {
+		logrus.Fatalf("error getting x509 source: %+v", err)
+	}
+	svid, err := source.GetX509SVID()
+	if err != nil {
+		logrus.Fatalf("error getting x509 svid: %+v", err)
+	}
+	log.FromContext(ctx).Infof("SVID: %q", svid.ID)
+
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("executing phase 3: creating icmp server ipam")
+	// ********************************************************************************
+
+	ipamChain := getIPAMChain(ctx, config.CidrPrefix)
+
+	log.FromContext(ctx).Infof("network prefixes parsed successfully")
+
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("executing phase 4: create icmp-server network service endpoint")
+	// ********************************************************************************
+
+	tokenServer := getSriovTokenServerChainElement(ctx)
+
+	responderEndpoint := endpoint.NewServer(ctx,
+		spiffejwt.TokenGeneratorFunc(source, config.MaxTokenLifetime),
+		endpoint.WithName(config.Name),
+		endpoint.WithAuthorizeServer(authorize.NewServer()),
+		endpoint.WithAdditionalFunctionality(
+			onidle.NewServer(ctx, cancel, config.IdleTimeout),
+			ipamChain,
+			policyroute.NewServer(newPolicyRoutesGetter(ctx, config.PBRConfigPath).Get),
+			recvfd.NewServer(),
+			mechanisms.NewServer(map[string]networkservice.NetworkServiceServer{
+				kernelmech.MECHANISM: kernel.NewServer(),
+				noop.MECHANISM:       null.NewServer(),
+			}),
+			tokenServer,
+			dnscontext.NewServer(config.DNSConfigs...),
+			sendfd.NewServer(),
+		),
+	)
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("executing phase 5: create grpc server and register icmp-server")
+	// ********************************************************************************
+	options := append(
+		tracing.WithTracing(),
+		grpc.Creds(
+			grpcfd.TransportCredentials(
+				credentials.NewTLS(
+					tlsconfig.MTLSServerConfig(source, source, tlsconfig.AuthorizeAny()),
+				),
+			),
+		),
+	)
+	server := grpc.NewServer(options...)
+	responderEndpoint.Register(server)
+	tmpDir, err := ioutil.TempDir("", config.Name)
+	if err != nil {
+		logrus.Fatalf("error creating tmpDir %+v", err)
+	}
+	defer func(tmpDir string) { _ = os.Remove(tmpDir) }(tmpDir)
+	listenOn := &(url.URL{Scheme: "unix", Path: filepath.Join(tmpDir, "listen.on")})
+	srvErrCh := grpcutils.ListenAndServe(ctx, listenOn, server)
+	exitOnErr(ctx, cancel, srvErrCh)
+	log.FromContext(ctx).Infof("grpc server started")
+
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("executing phase 6: register nse with nsm")
+	// ********************************************************************************
+	clientOptions := append(
+		tracing.WithTracingDial(),
+		grpc.WithBlock(),
+		grpc.WithDefaultCallOptions(grpc.WaitForReady(true)),
+		grpc.WithTransportCredentials(
+			grpcfd.TransportCredentials(
+				credentials.NewTLS(
+					tlsconfig.MTLSClientConfig(source, source, tlsconfig.AuthorizeAny()),
+				),
+			),
+		),
+	)
+
+	if config.RegisterService {
+		for _, serviceName := range config.ServiceNames {
+			nsRegistryClient := registryclient.NewNetworkServiceRegistryClient(ctx, &config.ConnectTo, registryclient.WithDialOptions(clientOptions...))
+			_, err = nsRegistryClient.Register(ctx, &registryapi.NetworkService{
+				Name:    serviceName,
+				Payload: config.Payload,
+			})
+
+			if err != nil {
+				log.FromContext(ctx).Fatalf("unable to register ns %+v", err)
+			}
+		}
+	}
+
+	nseRegistryClient := registryclient.NewNetworkServiceEndpointRegistryClient(
+		ctx,
+		&config.ConnectTo,
+		registryclient.WithDialOptions(clientOptions...),
+		registryclient.WithNSEAdditionalFunctionality(
+			registrysendfd.NewNetworkServiceEndpointRegistryClient(),
+		),
+	)
+	nse := getNseEndpoint(config, listenOn)
+	nse, err = nseRegistryClient.Register(ctx, nse)
+	logrus.Infof("nse: %+v", nse)
+
+	if err != nil {
+		log.FromContext(ctx).Fatalf("unable to register nse %+v", err)
+	}
+
+	// ********************************************************************************
+	log.FromContext(ctx).Infof("startup completed in %v", time.Since(starttime))
+	// ********************************************************************************
+
+	// wait for server to exit
+	<-ctx.Done()
+}
+
+func getNseEndpoint(config *Config, listenOn fmt.Stringer) *registryapi.NetworkServiceEndpoint {
+	nse := &registryapi.NetworkServiceEndpoint{
+		Name:                 config.Name,
+		NetworkServiceNames:  config.ServiceNames,
+		NetworkServiceLabels: make(map[string]*registryapi.NetworkServiceLabels),
+		Url:                  listenOn.String(),
+	}
+	for _, serviceName := range config.ServiceNames {
+		nse.NetworkServiceLabels[serviceName] = &registryapi.NetworkServiceLabels{Labels: config.Labels}
+	}
+	return nse
+}
+
+func getSriovTokenServerChainElement(ctx context.Context) (tokenServer networkservice.NetworkServiceServer) {
+	sriovTokens := tokens.FromEnv(os.Environ())
+	switch len(sriovTokens) {
+	case 0:
+		tokenServer = null.NewServer()
+	case 1:
+		var tokenKey string
+		for tokenKey = range sriovTokens {
+			break
+		}
+		tokenServer = token.NewServer(tokenKey)
+	default:
+		log.FromContext(ctx).Fatalf("endpoint must be configured with none or only one sriov resource")
+	}
+	return
+}
+
+func exitOnErr(ctx context.Context, cancel context.CancelFunc, errCh <-chan error) {
+	// If we already have an error, log it and exit
+	select {
+	case err := <-errCh:
+		log.FromContext(ctx).Fatal(err)
+	default:
+	}
+	// Otherwise wait for an error in the background to log and cancel
+	go func(ctx context.Context, errCh <-chan error) {
+		err := <-errCh
+		log.FromContext(ctx).Error(err)
+		cancel()
+	}(ctx, errCh)
+}
+
+// newPolicyRoutesGetter - watches the config file and dynamically updates policy routes.
+func newPolicyRoutesGetter(ctx context.Context, configPath string) *policyRoutesGetter {
+	p := &policyRoutesGetter{
+		ctx: ctx,
+	}
+	p.policyRoutes.Store([]*networkservice.PolicyRoute{})
+	updatePrefixes := func(bytes []byte) {
+		if bytes == nil {
+			p.policyRoutes.Store([]*networkservice.PolicyRoute{})
+		}
+		var source []*networkservice.PolicyRoute
+		err := yaml.Unmarshal(bytes, &source)
+		if err != nil {
+			log.FromContext(ctx).Errorf("Cannot unmarshal policies, err: %v", err.Error())
+			return
+		}
+		p.policyRoutes.Store(source)
+	}
+	updateCh := fs.WatchFile(p.ctx, configPath)
+	updatePrefixes(<-updateCh)
+	go func() {
+		for {
+			select {
+			case <-p.ctx.Done():
+				return
+			case update := <-updateCh:
+				updatePrefixes(update)
+			}
+		}
+	}()
+
+	return p
+}
+
+func (p *policyRoutesGetter) Get() []*networkservice.PolicyRoute {
+	return p.policyRoutes.Load().([]*networkservice.PolicyRoute)
+}
+
+type policyRoutesGetter struct {
+	ctx          context.Context
+	policyRoutes atomic.Value
+}
+
+func getIPAMChain(ctx context.Context, cIDRs []string) networkservice.NetworkServiceServer {
+	var ipamchain []networkservice.NetworkServiceServer
+	for _, cidr := range cIDRs {
+		var parseErr error
+		_, ipNet, parseErr := net.ParseCIDR(strings.TrimSpace(cidr))
+		if parseErr != nil {
+			log.FromContext(ctx).Fatalf("Could not parse CIDR %s; %+v", cidr, parseErr)
+		}
+		ipamchain = append(ipamchain, point2pointipam.NewServer(ipNet))
+	}
+	return chain.NewNetworkServiceServer(ipamchain...)
 }
